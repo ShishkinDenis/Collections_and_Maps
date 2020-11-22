3d5a4cedab16d6d086fca9c07d1611d5605695a7
ShishkinDenis <61963414+ShishkinDenis@users.noreply.github.com>
2020-11-22 11:42:11 +0300

    Layout

---
+++ app/src/main/java/ru/denisshishin/task3foxminded/CollectionsFragment.java
@@ -0,0 +1,18 @@
+package ru.denisshishin.task3foxminded;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CollectionsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_collections, container, false);
+    }
+}
