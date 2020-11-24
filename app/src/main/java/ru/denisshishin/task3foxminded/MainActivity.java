1293b1f6853e7c3c52d4fe8f07221c3a66db93af
ShishkinDenis <61963414+ShishkinDenis@users.noreply.github.com>
2020-11-24 23:35:47 +0300

    Rotation works

--- app/src/main/java/ru/denisshishin/task3foxminded/MainActivity.java
+++ app/src/main/java/ru/denisshishin/task3foxminded/MainActivity.java
@@ -1,14 +1,57 @@
 package ru.denisshishin.task3foxminded;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Collections;
+
+public class MainActivity extends FragmentActivity {
+    ViewPager2 viewPager;
+    TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(createCardAdapter());
+        new TabLayoutMediator(tabLayout,viewPager,new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0){
+                    tab.setText("Collections");
+                }
+                else{
+                    tab.setText("Maps");
+                }
+            }
+        })
+                .attach();
+
+
     }
+
+    private ViewPagerAdapter createCardAdapter() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        return adapter;
+    }
+
+
 }
