776310e67a5b154ae4fdd126c4cacb22a0419710
ShishkinDenis <61963414+ShishkinDenis@users.noreply.github.com>
2020-11-25 11:01:08 +0300

    Rotation works

--- app/src/main/java/ru/denisshishin/task3foxminded/CollectionsFragment.java
+++ app/src/main/java/ru/denisshishin/task3foxminded/CollectionsFragment.java
@@ -2,17 +2,56 @@ package ru.denisshishin.task3foxminded;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CollectionsFragment extends Fragment {
+import moxy.MvpAppCompatFragment;
+import moxy.presenter.InjectPresenter;
+
+public class CollectionsFragment extends MvpAppCompatFragment implements CollectionFragmentView {
+
+
+    @InjectPresenter
+    CollectionFragmentPresenter collectionFragmentPresenter;
+
+    TextView tvCollections;
+    Button btnCollectionsFragment;
+
+    //public CollectionsFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_collections, container, false);
+        View viewCollectionsFragment = inflater.inflate(R.layout.fragment_collections, container, false);
+        return viewCollectionsFragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnCollectionsFragment = view.findViewById(R.id.btnCollectionsFragment);
+        tvCollections = view.findViewById(R.id.tvCollections);
+
+        btnCollectionsFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                collectionFragmentPresenter.presentTvFragment();
+            }
+        });
+    }
+
+
+    @Override
+    public void showTvFragment() {
+        tvCollections.setText("Что-то");
+///перенести значение в Presenter
     }
 }
--- app/src/main/java/ru/denisshishin/task3foxminded/MapsFragment.java
+++ app/src/main/java/ru/denisshishin/task3foxminded/MapsFragment.java
@@ -2,18 +2,54 @@ package ru.denisshishin.task3foxminded;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import moxy.MvpAppCompatFragment;
+import moxy.presenter.InjectPresenter;
 
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends MvpAppCompatFragment implements MapsFragmentView {
+    @InjectPresenter
+    MapsFragmentPresenter mapsFragmentPresenter;
+
+    //public MapsFragment(){}
+    TextView tvMaps;
+    Button btnMapsFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        View viewMapsFragment = inflater.inflate(R.layout.fragment_maps, container, false);
+        return viewMapsFragment;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnMapsFragment = view.findViewById(R.id.btnMapsFragment);
+        tvMaps = view.findViewById(R.id.tvMaps);
+
+        btnMapsFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               mapsFragmentPresenter.presentTvMapsFragment();
+            }
+        });
+    }
+
+
+    @Override
+    public void showTvMapsFragment() {
+        tvMaps.setText("Что-то");
+///перенести значение в Presenter
     }
 }
